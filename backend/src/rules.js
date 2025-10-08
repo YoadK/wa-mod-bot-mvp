1893f0e392078e45c909a53859d03499bf979751
YoadK <yoadkot@gmail.com>
2025-10-08 22:02:01 +0300

    -Add more spam keywords to rules.js-Add URL shorteners to rules.js

--- backend/src/rules.js
+++ backend/src/rules.js
@@ -1,19 +1,19 @@
 const linkRe = /(https?:\/\/|chat\.whatsapp\.com\/?)/i;
-const shortenerRe = /(bit\.ly|t\.co|tinyurl\.com|goo\.gl)/i;
-const heKeywordsRe = /(הלווא|פורקס|קריפטו|הימור|סקס|xxx|porn|bet|forex)/i;
+const shortenerRe = /(bit\.ly|t\.co|tinyurl\.com|goo\.gl|ow.ly|short.link|cutt.ly|rebrand.ly)/i;
+const heKeywordsRe = /(הלווא|פורקס|קריפטו|הימור|סקס|xxx|porn|bet|forex|לוטו|קזינו|הימורים|loan|casino)/i;
 
 function evaluate({ text = '', type = 'unknown', joinedAgoMinutes = 9999, senderIsWhitelisted = false }) {
-  if (senderIsWhitelisted) return { action: 'allow', ruleId: 'whitelist' };
+    if (senderIsWhitelisted) return { action: 'allow', ruleId: 'whitelist' };
 
-  if (linkRe.test(text)) return { action: 'block', ruleId: 'r_link' };
-  if (shortenerRe.test(text)) return { action: 'block', ruleId: 'r_shortener' };
-  if (heKeywordsRe.test(text)) return { action: 'block', ruleId: 'r_kw' };
+    if (linkRe.test(text)) return { action: 'block', ruleId: 'r_link' };
+    if (shortenerRe.test(text)) return { action: 'block', ruleId: 'r_shortener' };
+    if (heKeywordsRe.test(text)) return { action: 'block', ruleId: 'r_kw' };
 
-  if (joinedAgoMinutes < 15 && type !== 'textMessage' && type !== 'extendedTextMessage') {
-    return { action: 'block', ruleId: 'r_probation_media' };
-  }
+    if (joinedAgoMinutes < 15 && type !== 'textMessage' && type !== 'extendedTextMessage') {
+        return { action: 'block', ruleId: 'r_probation_media' };
+    }
 
-  return { action: 'allow' };
+    return { action: 'allow' };
 }
 
 module.exports = { evaluate };
